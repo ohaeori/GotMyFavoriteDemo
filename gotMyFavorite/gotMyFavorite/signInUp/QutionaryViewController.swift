90b1a049a462573ad21200a8ac39b1d667d4974c
ohaeori <0typhoon416@gmail.com>
2020-06-15 17:57:47 +0900

    update

---
+++ gotMyFavorite/gotMyFavorite/signInUp/QutionaryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QutionaryViewController.swift
+//  gotMyFavorite
+//
+//  Created by ohaeori on 2020/06/15.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+
+import UIKit
+
+class QutionaryViewController: UIViewController {
+    @IBOutlet weak var dd: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
