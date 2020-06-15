b2a062f55278f68d291512f6237555757bc72750
ohaeori <0typhoon416@gmail.com>
2020-06-15 17:12:24 +0900

    update Quiz

--- gotMyFavorite/gotMyFavorite/MatchingViewController.swift
+++ gotMyFavorite/gotMyFavorite/MatchingViewController.swift
@@ -18,11 +18,15 @@ class MatchingViewController: UIViewController {
     var receivedID: String = ""
     var labelString: String = "none"
     var socket: SocketIOClient!
+    var msgArr: [String] = ["최첨단","역동적인","유머 있는","소름 돋는","학살 하는","경쟁 하는","환상적인","리듬감 있는","사랑스러운","d"]
     
     @IBAction func MatchingStart(_ sender: Any) {
         //change StoryBoard
         let storyboard: UIStoryboard = UIStoryboard(name: "Quiz", bundle: nil)
-        let next = storyboard.instantiateInitialViewController()
+        let next = storyboard.instantiateViewController(withIdentifier: "Quiz") as? QuestionViewController
+        
+        next?.quizArr = msgArr
+        next?.receivedID = self.receivedID
         next?.modalPresentationStyle = .fullScreen
         next?.modalTransitionStyle = .coverVertical
         self.present(next!, animated: true, completion: nil)
@@ -30,7 +34,9 @@ class MatchingViewController: UIViewController {
     @IBAction func ModifyFavorite(_ sender: Any) {
         //change StoryBoard
         let storyboard: UIStoryboard = UIStoryboard(name: "getFavorite", bundle: nil)
-        let next = storyboard.instantiateInitialViewController()
+        let next = storyboard.instantiateViewController(withIdentifier: "CVC") as? ColorViewController
+        
+        next?.receivedID = self.receivedID
         next?.modalPresentationStyle = .fullScreen
         next?.modalTransitionStyle = .coverVertical
         self.present(next!, animated: true, completion: nil)
